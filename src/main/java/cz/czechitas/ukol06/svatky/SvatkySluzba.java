49d657a12c4723f5b03a2afe6e9e707c324c5754
Anna Prymšová <anca7@centrum.cz>
2024-03-23 17:47:53 +0100

    Done, vyhledat svatky + test

--- src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
+++ src/main/java/cz/czechitas/ukol06/svatky/SvatkySluzba.java
@@ -1,13 +1,18 @@
 package cz.czechitas.ukol06.svatky;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.rmi.ServerError;
 import java.time.MonthDay;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class SvatkySluzba {
 
@@ -17,11 +22,20 @@ public class SvatkySluzba {
     private final Path cestaKDatum = Path.of("data/svatky.json");
     private final SeznamSvatku seznamSvatku;
 
-    public SvatkySluzba() throws IOException {
+    public SvatkySluzba() {
         // TODO načíst seznam svátků ze souboru svatky.json
-
-        // Následující řádek po vlastní implementaci smažete.
-        seznamSvatku = null;
+        try {
+            seznamSvatku = objectMapper.readValue(cestaKDatum.toFile(), SeznamSvatku.class);
+        } catch (StreamReadException e) {
+            System.err.println("StreamReadException in readValue - seznam svátků.");
+            throw new RuntimeException(e);
+        } catch (DatabindException e) {
+            System.err.println("DatabindException in readValue - seznam svátků.");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.err.println("IOException in readValue - seznam svátků.");
+            throw new RuntimeException(e);
+        }
     }
 
     public List<String> vyhledatSvatkyDnes() {
@@ -29,14 +43,11 @@ public class SvatkySluzba {
     }
 
     public List<String> vyhledatSvatkyKeDni(MonthDay day) {
-        // TODO
-        // získat seznam svátků
-        // převést na Stream
-        // pomocí metody filter() vybrat jen ty, které odpovídají zadanému dni (porovnat MonthDay pomocí metodyequals())
-        // pomocí metody map() získat z objektu jméno
-        // pomocí toList() převést na List
-
-        // Následující řádek po vlastní implementaci smažete.
-        return List.of();
+        List<Svatek> svatky = seznamSvatku.getSvatky();
+        return svatky
+                .stream() // převedení na Stream<Svatek>
+                .filter(svatek -> svatek.getDen().equals(day)) // vyfiltrovány jen svátky daného dne z parametru funkce
+                .map(Svatek::getJmeno) // získání jmen
+                .toList();
     }
 }
--- src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
+++ src/test/java/cz/czechitas/ukol06/svatky/SvatkySluzbaTest.java
@@ -2,12 +2,35 @@ package cz.czechitas.ukol06.svatky;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.time.MonthDay;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SvatkySluzbaTest {
 
     @Test
     void vyhledatSvatkyKeDni() {
-        //TODO implementovat test metody vyhledatSvatkyKeDni
+        SvatkySluzba svatkySluzba = new SvatkySluzba();
+        assertNotNull(svatkySluzba);
+
+        // test - správně vyhodnocený svátek jednoho jména daný den
+        List<String> svatekIvona = new ArrayList<>();
+        svatekIvona.add("Ivona");
+        assertEquals(svatekIvona, svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(3, 23)));
+        assertNotEquals(svatekIvona, svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(12, 24)));
+
+        // test - správně vyhodnocený svátek dvou jmen daný den
+        List<String> svatekPetrPavel = new ArrayList<>();
+        svatekPetrPavel.add("Petr");
+        svatekPetrPavel.add("Pavel");
+        assertEquals(svatekPetrPavel, svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(6, 29)));
+        assertNotEquals(svatekPetrPavel, svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(12, 24)));
+
+        // test - správně vyhodnoceno, že nemá svátek žádné jméno v daný den
+        List<String> svatekNikdo = new ArrayList<>();
+        assertEquals(svatekNikdo, svatkySluzba.vyhledatSvatkyKeDni(MonthDay.of(1, 1)));
     }
 }
